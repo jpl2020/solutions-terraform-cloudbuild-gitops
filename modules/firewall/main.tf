c59704efd8769799875a8008f309474a03cc93e1
jp25002 <juan.laudani@globant.com>
2021-06-08 22:39:55 -0300

    test2

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -25,7 +25,7 @@ resource "google_compute_firewall" "allow-http" {
     protocol = "tcp"
     ports    = ["80"]
   }
-
+/* cambio */
   target_tags   = ["http-server"]
   source_ranges = ["0.0.0.0/0"]
 }
