23f303db60c118db6b64af785baec199f4f9d9aa
jpl2020 <72326583+jpl2020@users.noreply.github.com>
2022-03-28 02:09:11 -0300

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable "project" {}
+variable "project" {
+  description = "Project"
+  type        = string
+}
